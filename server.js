98aac384bdb8a22d331b8fd00a65fd2f2e15ff46
Philippa Davies <philippadavies603@gmail.com>
2023-09-29 11:56:40 +1000

    delete existing data on start added

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const db = require('./config/connection');
-// Require model
-const { Genre } = require('./models');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
